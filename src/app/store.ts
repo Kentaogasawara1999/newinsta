e63eec6ec9409e45db857ca4118ad5980dbf3f3b
Kentaogasawara1999 <kentamanjapan@gmail.com>
2021-02-27 19:58:06 +0900

    first commit

---
+++ src/app/store.ts
@@ -0,0 +1,18 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import postReducer from "../features/post/postSlice";
+export const store = configureStore({
+  reducer: {
+    post: postReducer,
+    auth: authReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+export type AppDispatch = typeof store.dispatch;
